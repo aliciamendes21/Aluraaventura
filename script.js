462cd06ccef46da577094570ef488fbef5bd21a8
aliciamendes21 <0000111669167xsp@al.educacao.sp.gov.br>
2024-11-18 15:30:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+//Alícia Mendes 3D 29/09/2024
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
